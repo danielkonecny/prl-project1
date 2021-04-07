a154aacb4c66cc7bdb077299f6e3d3fb19ea70bc
Daniel Konecny <dkonecny97@gmail.com>
2021-04-07 23:27:18 +0200

    Fix test file

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$numbers = 16
-$processes = 5
+numbers=16
+processes=5
 
 mpic++ -o pms pms.cpp pms.h
 
-dd if=/dev/random bs=1 count=16 of=$numbers 1>/dev/null 2>/dev/null
+dd if=/dev/random bs=1 count=16 of=numbers 1>/dev/null 2>/dev/null
 
 mpirun --use-hwthread-cpus --oversubscribe -np $processes pms
 
