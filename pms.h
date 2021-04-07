81fd0cfae2a208cbf9efeee93439246e4e622fe5
Daniel Konecny <dkonecny97@gmail.com>
2021-04-07 23:15:26 +0200

    Refactor code

--- pms.h
+++ pms.h
@@ -18,3 +18,11 @@ vector<int> read_input(string file_name);
 void print_unsorted_numbers(vector<int> numbers);
 
 void print_sorted_numbers(vector<int> numbers);
+
+void check_and_reset_counters(int *upper_sent, int *lower_sent, int queue_length);
+
+vector<int> process_new_number(int process_id, int new_number, vector<int> sorted_numbers);
+
+vector<int> middle_processor(MPI_Status stat, int process_id, vector<int> sorted_numbers);
+
+int main (int argc, char *argv[]);
