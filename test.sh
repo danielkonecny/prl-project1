9c2ef16d0969fd71c3464299e0da6f4343f7a1b5
Daniel Konecny <dkonecny97@gmail.com>
2021-04-08 20:58:49 +0200

    Add documentation and finalize it

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ mpic++ -o $PROJECT $PROJECT.cpp $PROJECT.h
 
 dd if=/dev/random bs=1 count=$NUMBER_COUNT of=$NUMBER_FILE 1>/dev/null 2>/dev/null
 
-mpirun --use-hwthread-cpus --oversubscribe -np $PROCESS_COUNT $PROJECT
+# --use-hwthread-cpus --oversubscribe
+mpirun -np $PROCESS_COUNT $PROJECT
 
 rm -f $PROJECT $NUMBER_FILE
